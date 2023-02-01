55015502ffccf207263d76cbebbdef747259a9e9
Martynas <>
2023-02-01 13:26:27 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import CountryFilter from './CountryFilter';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
